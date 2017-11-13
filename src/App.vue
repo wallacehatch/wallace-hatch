c894f0f3782bace8ad3eff720c32075f6b58c5cb
gregm711 <gregm@wearevest.com>
2017-11-13 07:47:13 -0500

    setup project

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang='scss'>
+  @import './styles/main.scss';
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    min-height: 100%;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    position: relative;
+  }
 </style>
