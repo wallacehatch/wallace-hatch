cfbe5a5b1311deaebf068d1e8ab99c986fb41dea
gregm711 <gregm@wearevest.com>
2017-12-05 13:37:34 -0500

    updated update.sh;

--- update.sh
+++ update.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
 export NAME=kops.wallacehatch.com
 export KOPS_STATE_STORE=s3://wallace-hatch-kubernetes
 kops export kubecfg ${NAME}
 
 npm run build
-docker build -t wallace-hatch-backend .
+docker build -t wallace-hatch-frontend .
 docker tag wallace-hatch-frontend:latest 145054867171.dkr.ecr.us-east-1.amazonaws.com/wallace-hatch-frontend:latest
 docker push 145054867171.dkr.ecr.us-east-1.amazonaws.com/wallace-hatch-frontend:latest
-
 
 kubectl set image deployment/frontend-deployment frontend-container=145054867171.dkr.ecr.us-east-1.amazonaws.com/wallace-hatch-frontend:latest
