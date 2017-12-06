1e1c3b8e5522c659a2b14df75fe1eebeb270305f
gregm711 <gregm@wearevest.com>
2017-12-05 20:08:52 -0500

    updated countdown

--- update.sh
+++ update.sh
@@ -2,8 +2,9 @@
 export NAME=kops.wallacehatch.com
 export KOPS_STATE_STORE=s3://wallace-hatch-kubernetes
 kops export kubecfg ${NAME}
+buildCode=`openssl rand -hex 4` 
 npm run build
 docker build -t wallace-hatch-frontend .
-docker tag wallace-hatch-frontend:latest 145054867171.dkr.ecr.us-east-1.amazonaws.com/wallace-hatch-frontend:latest
-docker push 145054867171.dkr.ecr.us-east-1.amazonaws.com/wallace-hatch-frontend:latest
-kubectl set image deployment/frontend-deployment frontend-container=145054867171.dkr.ecr.us-east-1.amazonaws.com/wallace-hatch-frontend:latest
+docker tag wallace-hatch-frontend:latest 145054867171.dkr.ecr.us-east-1.amazonaws.com/wallace-hatch-frontend:$buildCode
+docker push 145054867171.dkr.ecr.us-east-1.amazonaws.com/wallace-hatch-frontend:$buildCode
+kubectl set image deployment/frontend-deployment frontend-container=145054867171.dkr.ecr.us-east-1.amazonaws.com/wallace-hatch-frontend:$buildCode
