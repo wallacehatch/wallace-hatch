7f61688e7b86845dccd676f590f13caa354da844
Ian Hansborough <ian.hansborough@gmail.com>
2017-12-06 09:54:20 -0500

    working on the experience section

--- src/components/home/ExperienceSection.vue
+++ src/components/home/ExperienceSection.vue
@@ -2,7 +2,15 @@
   <div class="experience-section-cont">
     <div class="quote-box">
       <p class="quote-text">“You can have anything you want in life if you dress for it.”</p>
-      
+      <p class="quote-source">– Edith Head</p>
+    </div>
+    <div class="section-heading-cont">
+      <p class="header">Life Experiences</p>
+      <p class="subtext">#WallaceHatch</p>
+      <hr class="line">
+    </div>
+    <div class="instagram-feed-cont">
+      <div class="instagram-image i0"></div>
     </div>
   </div>
 </template>
@@ -15,12 +23,27 @@ export default {
 
 <style lang="scss">
   @import '../../styles/_variables.scss';
+  .instagram-feed-cont {
+    margin-top: 4rem;
+    padding: 0 2.3rem;
+    @include respond-to(lg) {margin-top: 0rem;}
+    @include respond-to(md) {margin-top: 1.5rem;}
+    @include respond-to(sm) {margin-top: 2.5rem;}
+    .instagram-image {
+      background-size: contain;
+      background-position: center;
+      background-repeat: no-repeat;
+
+      &.i0 {
+        background-image: url('https://instagram.com/p/Bb2qU-4hVvn/media/');
+      }
+    }
+  }
   .experience-section-cont {
     text-align: center;
     .quote-box {
       display: inline-block;
       margin: 0 1rem;
-      margin-bottom: 6rem;
       max-width: 58.8rem;
       border: solid 2.4px #f1f1f1;
       padding: 4rem 8rem;
@@ -28,6 +51,19 @@ export default {
     }
     .quote-text {
       @include text-quote;
+      text-transform: uppercase;
+      @include respond-to(lg) {
+        font-size: 1.4rem;
+        line-height: 1.43;
+	      letter-spacing: 4px;
+      }
+    }
+    .quote-source {
+      @include intro-text;
+      font-size: 1.2rem;
+      margin-top: 1rem;
+      letter-spacing: 4px;
+      text-transform: uppercase;
     }
   }
 </style>
