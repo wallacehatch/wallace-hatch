a38c1938f03bc7cf4581ee3851cb639442f494bc
gregm711 <gregm@wearevest.com>
2017-12-06 12:47:06 -0500

    everything working

--- update.sh
+++ update.sh
@@ -5,6 +5,6 @@ kops export kubecfg ${NAME}
 buildCode=`openssl rand -hex 4` 
 npm run build
 docker build -t wallace-hatch-frontend .
-docker tag wallace-hatch-frontend:latest 145054867171.dkr.ecr.us-east-1.amazonaws.com/wallace-hatch-frontend:$buildCode
-docker push 145054867171.dkr.ecr.us-east-1.amazonaws.com/wallace-hatch-frontend:$buildCode
-kubectl set image deployment/frontend-deployment frontend-container=145054867171.dkr.ecr.us-east-1.amazonaws.com/wallace-hatch-frontend:$buildCode
+docker tag wallace-hatch-frontend:latest 145054867171.dkr.ecr.us-east-1.amazonaws.com/wallace-hatch-frontend:latest
+docker push 145054867171.dkr.ecr.us-east-1.amazonaws.com/wallace-hatch-frontend:latest
+kubectl set image deployment/frontend-deployment frontend-container=145054867171.dkr.ecr.us-east-1.amazonaws.com/wallace-hatch-frontend:latest
