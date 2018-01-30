9a47c2f53b6da2c92679cf60540ba285235a480a
gregm711 <gregm@wearevest.com>
2018-01-29 23:36:56 -0500

    diff api url

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://localhost:8081/"',
-  // API_URL: '"https://1ce21a3d.ngrok.io/"',
+  // API_URL: '"http://localhost:8081/"',
+  API_URL: '"https://1ce21a3d.ngrok.io/"',
 })
