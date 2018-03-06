d78a22e43be0db78e1d8c49fb529a00462554818
Ian Hansborough <ian.hansborough@gmail.com>
2018-03-06 11:16:19 -0500

    updated env

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"https://api.wallacehatch.com/"',
+  // API_URL: '"https://api.wallacehatch.com/"',
+  API_URL: '"https://api-staging.wallacehatch.com/"',
 }
