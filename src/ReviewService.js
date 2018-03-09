60df9c13406b409eb9db1efdd14ca98ec71f95a6
gregm711 <gregm@wearevest.com>
2018-03-09 12:06:47 -0500

    review service ready to go;

--- src/ReviewService.js
+++ src/ReviewService.js
@@ -7,21 +7,22 @@ svc.createReview = createReview;
 
 // returns a list of all reviews about a product
 function getProductReviews(productId) {
-	return Promise.resolve(axios.get(process.env.API_URL + 'get-product-reviews/' + String(id)))
+	return Promise.resolve(axios.get(process.env.API_URL + 'get-product-reviews/' + String(productId)))
 }
 
 // use to determine weather customer has previously purchased item and is allowed to create a review
-function validateReview(customerId, prodctId){
-    return Promise.resolve(axios.post(process.env.API_URL + 'validate-review/', {customer_id: customer_id, product_id: productId}))
+function validateReview(customerId, productId){
+    return Promise.resolve(axios.post(process.env.API_URL + 'validate-review/', {customer_id: customerId, product_id: productId}))
 }
 
-function createReview(rev){
-const review = {prodct_id: "prod_CCDBCRzlKEEp7V",
-              star_rating: 3.5,
-              review_title: "LOVE THIS WATCH",
-              review_message: "message about how great this f'in watch is ",
-              customer_id: "cus_CLeyoSgOmQdiDf",
-            }
+function createReview(review){
+// Sample review below 
+// const review = {prodct_id: "prod_CCDBCRzlKEEp7V",
+//               star_rating: 3.5,
+//               review_title: "LOVE THIS WATCH",
+//               review_message: "message about how great this f'in watch is ",
+//               customer_id: "cus_CLeyoSgOmQdiDf",
+//             }
  return Promise.resolve(axios.post(process.env.API_URL + 'create-review/', {product_id: review.prodct_id, star_rating: review.star_rating, review_title: review.review_title, review_message: review.review_message, customer_id: review.customer_id}))
 }
 
