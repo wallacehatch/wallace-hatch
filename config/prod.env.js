798df169c968af818ca24a879368067827ed7cf0
Ian Hansborough <ian.hansborough@gmail.com>
2018-03-12 09:22:21 -0400

    tracking vars set

---
+++ config/prod.env.js
@@ -0,0 +1,9 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  API_URL: '"https://api.wallacehatch.com/"',
+
+  // GA_ID: '"UA-115122323-1"' // staging GA tracking id
+  GA_ID: '"UA-115122323-2"' // Production GA tracking id
+
+}
