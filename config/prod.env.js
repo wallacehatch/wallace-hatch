fcd206f55b4538c3d5da249de984890641ad3d90
gregm711 <gregm@wearevest.com>
2018-03-14 11:42:01 -0400

    ok

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,9 +1,8 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"https://api-staging.wallacehatch.com/"',
-  GA_ID: '"UA-115122323-1"' // staging GA tracking id
-  // API_URL: '"https://api.wallacehatch.com/"',
-  // GA_ID: '"UA-115122323-2"' // Production GA tracking id
-
+  API_URL: '"https://api.wallacehatch.com/"',
+  // GA_ID: '"UA-115122323-1"' // staging GA tracking id
+  GA_ID: '"UA-115122323-2"', // Production GA tracking id
+  AD_ID: '"AW-812669170"'
 }
