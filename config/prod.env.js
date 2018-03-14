60900e4c485856cec99cfd0e61b333a248db9f5d
gregm711 <gregm@wearevest.com>
2018-03-14 11:43:14 -0400

    addded google ad to prod.js

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,6 +3,6 @@ module.exports = {
   NODE_ENV: '"production"',
   API_URL: '"https://api.wallacehatch.com/"',
   // GA_ID: '"UA-115122323-1"' // staging GA tracking id
-  GA_ID: '"UA-115122323-2"' // Production GA tracking id
-
+  GA_ID: '"UA-115122323-2"', // Production GA tracking id
+  AD_ID: '"AW-812669170"'
 }
