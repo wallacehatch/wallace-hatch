fcd1f1508fcfbfdd3ae685566dba040477602682
gregm711 <gregm@wearevest.com>
2018-03-14 13:39:11 -0400

    mobile almost done

--- src/components/common/instagramModal/Modal.vue
+++ src/components/common/instagramModal/Modal.vue
@@ -2,9 +2,18 @@
    <div v-show="lActive" id="insta_modal_mask" class="insta-modal-mask">
       <div class="insta-modal-cont">
          <div class="close-btn" @click="$emit('close')"><i class="fal fa-times"></i></div>
+         <div class="insta-content-top-cont-mobile sm-only">
+            <div class="oval">
+               <div class="logo"></div>
+            </div>
+            <div class="top-text">
+               <p><strong>wallacehatch</strong></p>
+               <p class="caption">{{instagramInfo.location}}</p>
+            </div>
+         </div>
          <div class="insta-image" :style="{backgroundImage: 'url(' + instagramInfo.picture_url + ')'}"></div>
          <div class="insta-content-cont">
-            <div class="insta-content-top-cont">
+            <div class="insta-content-top-cont hide-sm">
                <div class="oval">
                   <div class="logo"></div>
                </div>
@@ -16,7 +25,7 @@
             </div>
             <div class="insta-content-middle">
               <p class="caption"><strong>wallacehatch </strong>{{instagramInfo.caption}}</p>
-                <div class="products-cont"  v-for="(item, i) in instagramInfo.products">
+                <div class="products-cont hide-sm"  v-for="(item, i) in instagramInfo.products">
                     <product-tile :key="'pTile' + i" :item="item"></product-tile>
                 </div>
             </div>
@@ -111,9 +120,10 @@ export default {
   overflow: scroll;
   opacity: 0;
   @include respond-to(sm) {
-    padding-top: 8rem;
+    padding-top: 1.5rem;
   }
-}
+
+
 .insta-modal-cont {
   clear: both;
   max-width: 86rem;
@@ -124,8 +134,12 @@ export default {
   margin: auto;
   position: relative;
   overflow: auto;
-
   .insta-image{
+    @include respond-to(sm) {
+      height: 363px;
+      width: 100%;
+      margin-top: 1.9rem;
+    }
     clear: both;
     display: inline-block;
     background-position: center;
@@ -138,16 +152,73 @@ export default {
     width: 54%;
     height: 60rem;
   }
+  .insta-content-top-cont-mobile{
+
+    @include respond-to(sm) {
+    display: block !important;
+    overflow: auto;
+    padding-left: 1rem;
+    padding-top: 1.5rem;
+    .line{
+      overflow: auto;
+      height: 1px;
+      padding-top: 2rem;
+      width: 100%;
+      border-bottom: 1px solid #d8d8d8;
+  }
+  .top-text{
+      margin-top: 0.4rem;
+      margin-left: 2rem;
+      overflow: auto;
+      float: left;
+      display: inline-block;
+  }
+  .oval {
+  overflow: auto;
+  display: inline-block;
+  float: left;
+  width: 50px;
+  height: 50px;
+  background-color: #ffffff;
+  border: solid 1px #dbdbdb;
+  border-radius: 50%;
+}
+  .logo {
+
+    background-image: url('https://d3dty8fv62xana.cloudfront.net/wh-mark.svg');
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    width: 24px;
+    height: 21px;
+    object-fit: contain;
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+    margin: auto;
+  }
+}
+}
+
   .insta-content-cont {
+
     display: inline-block;
 
     width: 37.1%;
     padding: 3rem;
     @include respond-to(md) {width: 35%;}
+    @include respond-to(sm) {
+      width: 100%;
+      padding: 1.5rem;
+      margin: auto;
+    }
+  }
 
 
     .insta-content-top-cont{
       overflow: auto;
+      padding-left: 1rem;
+      padding-top: 1.5rem;
       .line{
         overflow: auto;
         height: 1px;
@@ -156,6 +227,7 @@ export default {
         border-bottom: 1px solid #d8d8d8;
     }
     .top-text{
+        margin-top: 0.4rem;
         margin-left: 2rem;
         overflow: auto;
         float: left;
@@ -172,6 +244,7 @@ export default {
     border-radius: 50%;
   }
     .logo {
+
       background-image: url('https://d3dty8fv62xana.cloudfront.net/wh-mark.svg');
       background-size: contain;
       background-position: center;
@@ -192,15 +265,21 @@ export default {
       font-size: 12px;
       line-height: 1.67;
       letter-spacing: 0.2px;
-      margin-bottom: 3rem;
+      margin-bottom: 2rem;
     }
     .products-cont{
     }
     text-align: left;
   }
-}
+
 
   .close-btn {
+
+    @include respond-to(sm) {
+      top: 0.8rem;
+      right: 1rem;
+      font-size: 28px;
+    }
     display: inline-block;
     position: absolute;
     top: 1.4rem;
@@ -214,5 +293,6 @@ export default {
 
   }
 
+}
 }
 </style>
