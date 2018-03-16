c5dca49273622fafcdb0112803fbf74353393e3d
gregm711 <gregm@wearevest.com>
2018-03-16 09:45:58 -0400

    texting now defualts to true

--- src/components/checkout/CheckoutRoot.vue
+++ src/components/checkout/CheckoutRoot.vue
@@ -47,7 +47,7 @@ export default {
           email: '',
           password: '',
           phone: '',
-          acceptTerms: false,
+          acceptTerms: true,
         },
         shipping: {
           name: '',
