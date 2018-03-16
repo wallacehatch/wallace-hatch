ddbfb4b92cdb278c84b0822afedf18cbdd71be6d
gregm711 <gregm@wearevest.com>
2018-03-16 09:29:17 -0400

    ok

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,8 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"https://api-staging.wallacehatch.com/"',
+  // API_URL: '"https://api-staging.wallacehatch.com/"',
+  API_URL: '"http://localhost:8090/"',
   GA_ID: '"UA-115122323-1"',
   AD_ID: '"AW-812669170"'
 })
